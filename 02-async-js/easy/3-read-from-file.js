4d3f2653c43252fc8dc6d8ba5cd6638ec70a50e3
aamirmabs <aamir@ogaamyr.com>
2025-04-10 12:06:06 +0100

    3-read-from-file.js done

---
+++ 02-async-js/easy/3-read-from-file.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+// fs
+fs.readFile(`./text.txt`, 'utf-8', (err, data) => {
+  if (err) {
+    console.error("Error reading file");
+    return;
+  }
+
+  console.log("--- CONTENTS ---");
+  console.log(data);
+})
+
+// expensive task
+sum = 0;
+for (let i = 0; i < 10000000; i++) {
+  sum += i;
+}
+console.log("AFTER: ", sum);
